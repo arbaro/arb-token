89c52a003449512403eb3805d66f9b1ab01da02f
John Williamson <johnjwilliamson@live.com>
2019-03-05 12:28:58 +1000

    initial commit

---
+++ contract/arbaroToken.cpp
@@ -0,0 +1,47 @@
+#include "./arbaroToken.hpp"
+
+using namespace eosio;
+using namespace std;
+
+void arbaroToken::init(name name)
+{
+    require_auth(_self);
+}
+
+void arbaroToken::transfer(name from, name to, asset quantity, string memo)
+{
+    if (from == _self)
+    {
+        // we're sending money, do nothing additional
+        return;
+    }
+
+    eosio_assert(to == _self, "contract is not involved in this transfer");
+    eosio_assert(quantity.symbol.is_valid(), "invalid quantity");
+    eosio_assert(quantity.amount > 0, "only positive quantity allowed");
+    eosio_assert(quantity.symbol == EOS_SYMBOL, "only EOS tokens allowed");
+}
+
+void arbaroToken::testreset()
+{
+    require_auth(_self);
+    // auto itr = games.begin();
+    // while(itr != games.end()) {
+    //     itr = games.erase(itr);
+    // }
+}
+
+extern "C" void apply(uint64_t receiver, uint64_t code, uint64_t action)
+{
+    if (code == "eosio.token"_n.value && action == "transfer"_n.value)
+    {
+        eosio::execute_action(eosio::name(receiver), eosio::name(code), &arbaroToken::transfer);
+    }
+    else if (code == receiver)
+    {
+        switch (action)
+        {
+            EOSIO_DISPATCH_HELPER(arbaroToken, (init)(testreset))
+        }
+    }
+}
---
+++ contract/arbaroToken.hpp
@@ -0,0 +1,23 @@
+#include <string>
+
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/asset.hpp>
+
+#define EOS_SYMBOL symbol("EOS", 4)
+
+CONTRACT arbaroToken : public eosio::contract
+{
+    using contract::contract;
+
+  public:
+    struct init
+    {
+        init(){};
+        eosio::name name;
+        EOSLIB_SERIALIZE(init, (name))
+    };
+
+    ACTION init(eosio::name name);
+    ACTION testreset();
+    void transfer(eosio::name from, eosio::name to, eosio::asset quantity, std::string memo);
+};
